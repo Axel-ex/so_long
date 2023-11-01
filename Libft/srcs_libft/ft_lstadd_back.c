dc57762e942b56a79c34ee68477099d26c0e6185
Axel <Axel@macbook-air.home>
2023-11-01 17:52:43 +0100

    Libft update

--- Libft/srcs_libft/ft_lstadd_back.c
+++ Libft/srcs_libft/ft_lstadd_back.c
@@ -3,26 +3,26 @@
 /*                                                        :::      ::::::::   */
 /*   ft_lstadd_back.c                                   :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: axel <marvin@42.fr>                        +#+  +:+       +#+        */
+/*   By: achabrer <achabrer@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/23 12:07:04 by axel              #+#    #+#             */
-/*   Updated: 2023/04/23 12:09:28 by axel             ###   ########.fr       */
+/*   Updated: 2023/11/01 16:43:37 by achabrer         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-void	ft_lstadd_back(t_list **lst, t_list *new)
+void	ft_lstadd_back(t_list **lst, t_list *new_node)
 {
 	t_list	*tmp;
 
-	if (!lst || !new)
+	if (!lst || !new_node)
 		return ;
 	if (*lst == NULL)
-		*lst = new;
+		*lst = new_node;
 	else
 	{
 		tmp = ft_lstlast(*lst);
-		tmp->next = new;
+		tmp->next = new_node;
 	}
 }
--- Libft/srcs_libft/ft_lstadd_front.c
+++ Libft/srcs_libft/ft_lstadd_front.c
@@ -3,17 +3,17 @@
 /*                                                        :::      ::::::::   */
 /*   ft_lstadd_front.c                                  :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: axelc <achabrer@student.42porto.com>       +#+  +:+       +#+        */
+/*   By: achabrer <achabrer@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/13 16:16:13 by axelc             #+#    #+#             */
-/*   Updated: 2023/03/13 16:21:42 by axelc            ###   ########.fr       */
+/*   Updated: 2023/11/01 16:43:30 by achabrer         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-void	ft_lstadd_front(t_list **lst, t_list *new)
+void	ft_lstadd_front(t_list **lst, t_list *new_node)
 {
-	new->next = *lst;
-	*lst = new;
+	new_node->next = *lst;
+	*lst = new_node;
 }
